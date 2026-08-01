ae9b6da7da62fe5818ade351a7a9fe04440bcdd5
adrienylaiz-alt <adrienylaiz@gmail.com>
2026-07-31 21:23:10 -0300

    Reorganiza arquivos em novas pastas

---
+++ AULA_24_07/main.py
@@ -0,0 +1,5 @@
+from AULA_24_07.args import somar
+from AULA_24_07.contador import incrementar
+
+resultado_soma=somar(4, 7)
+print(incrementar(resultado_soma))
--- main.py
+++
@@ -1,5 +0,0 @@
-from args import somar
-from contador import incrementar
-
-resultado_soma=somar(4, 7)
-print(incrementar(resultado_soma))
